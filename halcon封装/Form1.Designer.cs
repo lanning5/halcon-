d0f3827c92b13be93b1b2264ec9db9bbb1532c4b
lanning <xinhao.liang@cygia.com>
2020-08-24 11:22:38 +0800

    添加项目文件。

---
+++ halcon封装/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace halcon封装
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hSmartWindowControl1 = new HalconDotNet.HSmartWindowControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hSmartWindowControl1
+            // 
+            this.hSmartWindowControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.hSmartWindowControl1.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.hSmartWindowControl1.HDoubleClickToFitContent = true;
+            this.hSmartWindowControl1.HDrawingObjectsModifier = HalconDotNet.HSmartWindowControl.DrawingObjectsModifier.None;
+            this.hSmartWindowControl1.HImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hSmartWindowControl1.HKeepAspectRatio = true;
+            this.hSmartWindowControl1.HMoveContent = true;
+            this.hSmartWindowControl1.HZoomContent = HalconDotNet.HSmartWindowControl.ZoomContent.WheelForwardZoomsIn;
+            this.hSmartWindowControl1.Location = new System.Drawing.Point(20, 9);
+            this.hSmartWindowControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.hSmartWindowControl1.Name = "hSmartWindowControl1";
+            this.hSmartWindowControl1.Size = new System.Drawing.Size(596, 512);
+            this.hSmartWindowControl1.TabIndex = 0;
+            this.hSmartWindowControl1.WindowSize = new System.Drawing.Size(596, 512);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(693, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(693, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "处理图片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(693, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "下一张";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(693, 275);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 57);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "打开任意图片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(693, 356);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 57);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "拟合圆";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(693, 437);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(158, 57);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "中心点";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 529);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hSmartWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HSmartWindowControl hSmartWindowControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ halcon封装/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace halcon封装
+{
+    public partial class Form1 : Form
+    {
+        HDevelopExport hd = new HDevelopExport();
+        HObject ho_image;
+        // 图片
+        string address;
+        int Addindex = 0;
+        string[] addressArray = { "../../../../image/", ".bmp.tif" };
+        int[] addressIndex = { 0, 1, 2, 3, 4 };
+        private static HWindow hwin; //全局窗口变量
+        private void getAddress()
+        {
+            address = addressArray[0] + addressIndex[Addindex] + addressArray[1];
+        }
+        // 图片缩放
+        private void my_MouseWheel(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point pt = this.Location;
+            int leftBorder = hSmartWindowControl1.Location.X;
+            int rightBorder = hSmartWindowControl1.Location.X + hSmartWindowControl1.Size.Width;
+            int topBorder = hSmartWindowControl1.Location.Y;
+            int bottomBorder = hSmartWindowControl1.Location.Y + hSmartWindowControl1.Size.Height;
+            if (e.X > leftBorder && e.X < rightBorder && e.Y > topBorder && e.Y < bottomBorder)
+            {
+                MouseEventArgs newe = new MouseEventArgs(e.Button, e.Clicks,
+                                                     e.X - pt.X, e.Y - pt.Y, e.Delta);
+                hSmartWindowControl1.HSmartWindowControl_MouseWheel(sender, newe);
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            getAddress();
+            hwin = hSmartWindowControl1.HalconWindow;
+            hd.InitHalcon(hwin);
+            // 图片缩放订阅监听
+            this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.my_MouseWheel);
+        }
+        // 读取图片
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            hd.readImage(out ho_image, address);
+        }
+        // 处理图片
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ho_image == null)
+            {
+                MessageBox.Show("当前未打开图片");
+                return;
+            }
+            hd.dealImage(ho_image);
+
+        }
+        // 下一张
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Addindex == addressIndex.Length - 1)
+            {
+                Addindex = 0;
+            }
+            else Addindex++;
+            getAddress();
+            hd.readImage(out ho_image, address);
+        }
+        // 读取任意图片
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            address = ofd.FileName;
+            hd.readImage(out ho_image, address);
+        }
+        // 拟合圆
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (ho_image == null)
+            {
+                MessageBox.Show("当前未打开图片");
+                return;
+            }
+            hd.getCircle(ho_image);
+        }
+        // 中心点
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (ho_image == null)
+            {
+                MessageBox.Show("当前未打开图片");
+                return;
+            }
+            hd.getCenterPoint(ho_image);
+        }
+    }
+}
